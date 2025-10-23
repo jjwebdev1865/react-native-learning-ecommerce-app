b0fab7004fa39356b8e3b158ecd26e0f5250df7b
Jim Jiracek <james.jiracek@slalom.com>
2025-10-23 13:08:14 -0400

    list of home page products

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,19 +1,33 @@
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import React from "react";
 import AppSafeView from "../../components/views/AppSafeView";
 import HomeHeader from "../../components/headers/HomeHeader";
 import ProductCard from "../../components/cards/ProductCard";
+import { products } from "../../data/products";
+import { scale, verticalScale } from "react-native-size-matters";
 
 const HomeScreen = () => {
   return (
     <AppSafeView>
       <HomeHeader />
 
-      <ProductCard
-        title="iPhone 15"
-        price="1200"
-        imageUrl=""
-        onPress={() => console.log("pressed")}
+      <FlatList
+        numColumns={2}
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ProductCard
+            title={item.title}
+            price={item.price.toString()}
+            imageUrl={item.imageURL}
+            onPress={() => console.log("pressed", item.title)}
+          />
+        )}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          marginBottom: verticalScale(10),
+        }}
+        contentContainerStyle={{ paddingHorizontal: scale(10) }}
       />
     </AppSafeView>
   );
