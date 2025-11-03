fe4e42ad1cbc92d3383b1b8cd79873d9121bcc14
Jim Jiracek <james.jiracek@slalom.com>
2025-11-03 08:53:03 -0500

    section 9 - sign up validation

--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,50 +1,83 @@
 import { Image, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React from "react";
 import AppSafeView from "../../components/views/AppSafeView";
 import { sharedPaddingHorizontal } from "../../styles/sharedStyles";
 import { IMAGES } from "../../constants/image-paths";
 import { scale, verticalScale } from "react-native-size-matters";
-import AppTextInput from "../../components/inputs/AppTextInput";
-import AppText from "../../components/texts/AppText";
 import AppButton from "../../components/buttons/AppButton";
 import { AppColors } from "../../styles/colors";
 import { useNavigation } from "@react-navigation/native";
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import AppTextInputController from "../../components/inputs/AppTextInputController";
+import { useForm } from "react-hook-form";
+
+const schema = yup
+  .object({
+    userName: yup
+      .string()
+      .required("User Name is required")
+      .min(6, "User Name is too short"),
+    email: yup
+      .string()
+      .required("Email is required")
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Invalid email format"
+      )
+      .min(6, "Email is too short"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(8, "Password is too short"),
+  })
+  .required();
+
+type FormData = yup.InferType<typeof schema>;
+
 const SignUpScreen = () => {
   const navigation = useNavigation();
-  
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleSignUp = (data: FormData) => {
+    console.log("Create New Account pressed");
+  };
 
   return (
     <AppSafeView style={styles.container}>
       <Image source={IMAGES.appLogo} style={styles.logo} />
-      <AppTextInput
-        placeholder="User Name"
-        value={username}
-        onChangeText={setUsername}
+
+      <AppTextInputController
+        placeholder="User name"
+        name="userName"
+        control={control}
       />
-      <AppTextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <AppTextInput
+      <AppTextInputController
+        placeholder="Email"
+        name="email"
+        control={control}
+      />
+      <AppTextInputController
         placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
+        name="password"
+        control={control}
       />
-      <AppText style={styles.appName}>Smart E-Commerce</AppText>
 
       <AppButton
         title="Create New Account"
-        onPress={() => {
-          console.log("Create New Account pressed");
-        }}
+        onPress={handleSubmit(handleSignUp)}
       />
       <AppButton
         title="Go To Sign In"
         style={styles.goToSignInButton}
         textColor={AppColors.primary}
-        onPress={() => {navigation.navigate("SignInScreen")}}
+        onPress={() => {
+          navigation.navigate("SignInScreen");
+        }}
       />
     </AppSafeView>
   );
