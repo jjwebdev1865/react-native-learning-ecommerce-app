a04b16e7ed1752d4b4692e642c548f1aa033197a
Jim Jiracek <james.jiracek@slalom.com>
2025-11-14 08:48:45 -0500

    storing language to local storage for persistence

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./en.json";
 import de from "./de.json";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LANGUAGES = {
   en: {
@@ -13,17 +14,44 @@ const LANGUAGES = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources: LANGUAGES,
-  fallbackLng: "en",
-  defaultNS: "translation",
-  ns: ["translation"],
-  react: {
-    useSuspense: false,
+// For storing with local state
+const LANGUAGE_DETECTOR = {
+  type: "languageDetector" as const,
+  async: true,
+  detect: async (callback: (lang: string) => void) => {
+    try {
+      const savedData = await AsyncStorage.getItem("LANGUAGE");
+      const lng = savedData ? savedData : "en";
+      callback(lng);
+    } catch (error) {
+      console.error("Error fetching language preference:", error);
+      callback("en");
+    }
   },
-  interpolation: {
-    escapeValue: false,
+  // init: () => {},
+  cacheUserLanguage: async (lang: string) => {
+    try {
+      await AsyncStorage.setItem("LANGUAGE", lang);
+    } catch (error) {
+      console.error("Error saving language preference:", error);
+    }
   },
-});
+};
+
+i18n
+  .use(LANGUAGE_DETECTOR)
+  .use(initReactI18next)
+  .init({
+    resources: LANGUAGES,
+    fallbackLng: "en",
+    defaultNS: "translation",
+    ns: ["translation"],
+    react: {
+      useSuspense: false,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
